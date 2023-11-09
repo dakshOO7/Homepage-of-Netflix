085f65cc0eff8c29da983b189a329e48f8401c7b
Daksh Mohan <121113388+dakshOO7@users.noreply.github.com>
2023-11-09 20:21:11 +0530

    Add files via upload

---
+++ Netflix Project/netflix.js
@@ -0,0 +1,5 @@
+// document.getElementById("Hrithik").addEventListener("click", function () {
+//     var vidmute = document.getElementById("Hrithik")
+//     vidmute.muted = false
+//     vidmute.play();
+// })
